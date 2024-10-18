110ca0858b049a1526555ebfe1e42fa360c03cdb
akmadan <akmadan1717@gmail.com>
2024-10-18 15:27:01 +0530

    init

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,10 @@ const app = express();
 app.use(cors());
 
 // Define the endpoint for the cron job to call
-const MAIN_SERVER_URL = "https://prod-ts-liveliness-server.onrender.com/api/session/cron/job"; // Replace with your actual server's endpoint
+const MAIN_SERVER_URL =
+  "https://prod-ts-liveliness-server.onrender.com/api/session/cron/job"; // Replace with your actual server's endpoint
 
-// Set up the cron job to run every 24 hours at 9 AM IST (3:30 AM UTC)
-cron.schedule("30 3 * * *", async () => {
+const runJob = async () => {
   console.log("Cron job triggered. Making GET request to main server...");
 
   try {
@@ -22,9 +22,10 @@ cron.schedule("30 3 * * *", async () => {
   } catch (error) {
     console.error("Error making GET request to main server:", error.message);
   }
-});
+};
 
 // Start the cron server on a port
 app.listen(4000, () => {
   console.log("Cron server is running on Port 4000");
+  runJob();
 });
