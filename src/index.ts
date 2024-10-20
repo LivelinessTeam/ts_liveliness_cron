64c6c1f9f6e9cdb1a4f93dc757da0e182e1bb097
akmadan <akmadan1717@gmail.com>
2024-10-21 01:23:57 +0530

    init

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,6 @@ import * as cron from "node-cron";
 import * as cors from "cors";
 import axios from "axios";
 
-const app = express();
-
-// Express Configuration
-app.use(cors());
-
 // Define the endpoint for the cron job to call
 const MAIN_SERVER_URL =
   "https://prod-ts-liveliness-server.onrender.com/api/session/cron/job"; // Replace with your actual server's endpoint
@@ -19,13 +14,11 @@ const runJob = async () => {
     // Make a GET request to your main server's endpoint
     const response = await axios.get(MAIN_SERVER_URL);
     console.log("Response from main server:", response.data);
+    process.exit(0);
   } catch (error) {
     console.error("Error making GET request to main server:", error.message);
+    process.exit(1);
   }
 };
 
-// Start the cron server on a port
-app.listen(4000, () => {
-  console.log("Cron server is running on Port 4000");
-  runJob();
-});
+runJob();
